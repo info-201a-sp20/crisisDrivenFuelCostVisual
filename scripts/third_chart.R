37e955652e47559d0e1757a18c86a01f1d22ec00
rimmy <estwst2352@gmail.com>
2020-05-24 00:41:46 +0900

    added

--- scripts/third_chart.R
+++ scripts/third_chart.R
@@ -8,11 +8,12 @@ library(ggplot2)
 
 
 graph_third <- function(df) {
-new_data <- data %>% filter(Code == "USA")
+new_data <- data %>% filter(Code == "USA" | Code =="KOR" | Code == "AUS" | Code == "BEL" | Code == "CAN" | Code == "CHN" | Code == "FRA" | Code == "IND" | Code == "JPN" | Code == "RUS")
 
 oil_consumption <- ggplot(data = new_data, aes(x = Year, y = Oil)) + geom_point() + scale_x_log10() +
   ggtitle("Oil Consumption By Country") + theme(plot.title = element_text(family = "serif", face = "bold", hjust = 0.5, size = 15, color = "brown")) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5 )) + labs(x="Years", y="Oil Consumption \n (Thousand barrels per day)") + facet_wrap(~Entity) 
+
 
 return(oil_consumption)
 
