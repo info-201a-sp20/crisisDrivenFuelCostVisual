9a4486691bd7bf97be4943c36b2921354cceeeb0
rimmy <estwst2352@gmail.com>
2020-05-24 00:26:06 +0900

    fixedd

--- scripts/second_chart.R
+++ scripts/second_chart.R
@@ -7,11 +7,12 @@
 
 library(ggplot2)
 
-graph_second <- function(df) { 
-gas_prod <- read.csv(file = "../data/fossilfuel/fossil-fuel-price-index.csv",stringsAsFactors = FALSE)
+#gas_prod <- read.csv(file = "../data/fossilfuel/fossil-fuel-price-index.csv",stringsAsFactors = FALSE)
+graph_second <- function(df) {
+
 years_x <- c(1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015)
 
-gg <- ggplot(data=gas_prod, aes(x=Year , y = Coal, color = "blue"))
+gg <- ggplot(data=df, aes(x=Year , y = Coal, color = "blue"))
 
 result <- gg + geom_line(aes(x=Year, y = Coal, color = "blue")) + geom_line(aes(x = Year, y = Oil, color = "red")) + 
   geom_line(aes(x = Year, y = NaturalGas, color = "black")) + labs(x="Years", y="Price in Dollar") + 
@@ -19,6 +20,7 @@ result <- gg + geom_line(aes(x=Year, y = Coal, color = "blue")) + geom_line(aes(
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5 )) + theme(legend.position="right") +
   scale_color_manual(name = "Fossil Fuel Type",  values = c("blue", "red", "black"), labels = c("blue" = "Coal", "red" = "Oil", "black" = "NaturalGas"))
 
-  result
+  return(result)
+
 }
 
--- scripts/third_chart.R
+++ scripts/third_chart.R
@@ -1,27 +1,20 @@
-#This chart describes fossil fuel consumption by country.
-#Three charts are included.
-#One for gas, one for coal, and one for oil.
+#This chart describes fossil fuel consumption by country, especially in Oil.
 #It is showing each of consumptions by each year by each countires. 
 
 library(ggplot2)
-graph_third(df) {
-ff_consumption <- read.csv(file = "../data/fossilfuel/fossil-fuel-consumption.csv",stringsAsFactors = FALSE)
 
-gas_consumption <- ggplot(ff_consumption, aes(x = Year, y = Gas)) + geom_point() + scale_x_log10() +
-  ggtitle("Gas Consumption By Country") + theme(plot.title = element_text(family = "serif", face = "bold", hjust = 0.5, size = 15, color = "red")) +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5 )) + labs(x="Years", y="Gas Consumption)") + facet_wrap(~Entity) 
-
-coal_consumption <- ggplot(ff_consumption, aes(x = Year, y = Coal)) + geom_point() + scale_x_log10() +
-  ggtitle("Coal Consumption By Country") + theme(plot.title = element_text(family = "serif", face = "bold", hjust = 0.5, size = 15, color = "blue")) +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5 )) + labs(x="Years", y="Coal Consumption") + facet_wrap(~Entity) 
+#ff_consumption <- read.csv(file = "../data/fossilfuel/fossil-fuel-consumption.csv",stringsAsFactors = FALSE)
 
 
-oil_consumption <- ggplot(ff_consumption, aes(x = Year, y = Oil)) + geom_point() + scale_x_log10() +
+
+graph_third <- function(df) {
+new_data <- data %>% filter(Code == "USA")
+
+oil_consumption <- ggplot(data = new_data, aes(x = Year, y = Oil)) + geom_point() + scale_x_log10() +
   ggtitle("Oil Consumption By Country") + theme(plot.title = element_text(family = "serif", face = "bold", hjust = 0.5, size = 15, color = "brown")) +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5 )) + labs(x="Years", y="Oil Consumption )") + facet_wrap(~Entity) 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5 )) + labs(x="Years", y="Oil Consumption \n (Thousand barrels per day)") + facet_wrap(~Entity) 
 
+return(oil_consumption)
 
-gas_consumption
-coal_consumption
-oil_consumption
 }
+
