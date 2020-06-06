c46e4b864ae2b42a6d0ebf383db1c5c3e0ec399e
rimmy <estwst2352@gmail.com>
2020-06-06 10:56:02 +0900

    price chart added

--- app_server.R
+++ app_server.R
@@ -2,6 +2,50 @@
 server <- function(input, output) {
   ## Hearim your graph goes here
   
+  ## Output an year..? graph..
+  
+    cons_data <- read.csv("data/fossilfuel/fossil-fuel-price-index.csv") %>%
+      subset(select = -c(Entity, Code)) %>%
+      
+      output$newbounce <- renderPlot({
+        if(input$radioInput == 1){
+          new_data <- filter(cons_data, Year >= 1970 & Year < 1980) %>%
+            melt(id.vars = 'Dollar')
+          result <- ggplot(data=new_data, aes(x=Year , y = Oil)) + 
+            geom_line(aes(x=Year, y = Coal, color = "blue")) + 
+            geom_line(aes(x = Year, y = Oil, color = "red")) + 
+            geom_line(aes(x = Year, y = NaturalGas, color = "black")) + 
+            scale_x_continuous(breaks = pretty(cons_data$Year, n = 10)) +
+            labs(x="Years", y="Price in Dollar", title= "70s energy crisis")
+          result
+        }else if(input$radioInput == 2){
+          new_data <- filter(data_bounce, Year >= 1980 & Year < 1990) %>%
+            melt(id.vars = 'Dollar')
+          result <- ggplot(data=new_data, aes(x=Year , y = Oil)) + 
+            geom_line(aes(x=Year, y = Coal, color = "blue")) + 
+            geom_line(aes(x = Year, y = Oil, color = "red")) + 
+            geom_line(aes(x = Year, y = NaturalGas, color = "black")) + 
+            scale_x_continuous(breaks = pretty(cons_data$Year, n = 10)) +
+            labs(x="Years", y="Price in Dollar", title= "80s energy crisis")
+        }else if(input$radioInput == 3){
+          new_data <- filter(data_bounce, Year >= 2007 & Year < 2017) %>%
+            melt(id.vars = 'Dollar')
+          result <- ggplot(data=new_data, aes(x=Year , y = Oil)) + 
+            geom_line(aes(x=Year, y = Coal, color = "blue")) + 
+            geom_line(aes(x = Year, y = Oil, color = "red")) + 
+            geom_line(aes(x = Year, y = NaturalGas, color = "black")) + 
+            scale_x_continuous(breaks = pretty(cons_data$Year, n = 10)) +
+            labs(x="Years", y="Price in Dollar", title= "Great Recession")
+          result
+        } 
+        })     
+      
+    
+
+  
+  
+  
+  
   
   ## Consumption graph
   output$consumption <- renderPlot({
