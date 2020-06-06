71c3428181d38a862ca3c06483f02e80999935fd
rimmy <estwst2352@gmail.com>
2020-06-06 12:06:36 +0900

    fixed all

--- app_server.R
+++ app_server.R
@@ -1,32 +1,33 @@
 
 server <- function(input, output) {
+  
+  cons_data <- read.csv("data/fossilfuel/fossil-fuel-price-index.csv") %>%
+    subset(select = -c(Entity, Code))
+  
       output$newbounce <- renderPlot({
-        cons_data <- read.csv("data/fossilfuel/fossil-fuel-price-index.csv") %>%
-          subset(select = -c(Entity, Code)) %>%
-          
-          
         if(input$radioInput == 1){
-          new_data <- filter(cons_data, Year >= 1970 & Year < 1980) %>%
-            melt(id.vars = 'Dollar')
+          new_data <- filter(cons_data, Year >= 1989 & Year < 1999) %>%
+            melt(id.vars = 'Year')
           result <- ggplot(data=cons_data, aes(x=Year , y = Oil)) + 
             geom_line(aes(x=Year, y = Coal, color = "blue")) + 
             geom_line(aes(x = Year, y = Oil, color = "red")) + 
             geom_line(aes(x = Year, y = NaturalGas, color = "black")) + 
             scale_x_continuous(breaks = pretty(cons_data$Year, n = 10)) +
-            labs(x="Years", y="Price in Dollar", title= "70s energy crisis")
+            labs(x="Years", y="Price in Dollar", title= "1990 Oil Price Shock")
           result
         }else if(input$radioInput == 2){
-          new_data <- filter(cons_data, Year >= 1980 & Year < 1990) %>%
-            melt(id.vars = 'Dollar')
+          new_data <- filter(cons_data, Year >= 1999 & Year < 2010) %>%
+            melt(id.vars = 'Year')
           result <- ggplot(data=cons_data, aes(x=Year , y = Oil)) + 
             geom_line(aes(x=Year, y = Coal, color = "blue")) + 
             geom_line(aes(x = Year, y = Oil, color = "red")) + 
             geom_line(aes(x = Year, y = NaturalGas, color = "black")) + 
             scale_x_continuous(breaks = pretty(cons_data$Year, n = 10)) +
-            labs(x="Years", y="Price in Dollar", title= "80s energy crisis")
+            labs(x="Years", y="Price in Dollar", title= "2000s energy crisis")
+          result
         }else if(input$radioInput == 3){
-          new_data <- filter(cons_data, Year >= 2007 & Year < 2017) %>%
-            melt(id.vars = 'Dollar')
+          new_data <- filter(cons_data, Year >= 2010) %>%
+            melt(id.vars = 'Year')
           result <- ggplot(data=cons_data, aes(x=Year , y = Oil)) + 
             geom_line(aes(x=Year, y = Coal, color = "blue")) + 
             geom_line(aes(x = Year, y = Oil, color = "red")) + 
